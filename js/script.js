f02858ac9838c7c2b560f414c5eee7c8c0370fe6
Aj <deajetrott@gmail.com>
2019-11-20 12:44:10 -0400

    bug fixes & consistency

--- js/script.js
+++ js/script.js
@@ -1,30 +1,56 @@
-// responsive()
-// $("#menuLink").on("click",function(){
-// responsive()
+// page loads
+// call function to check for cookie
+// check for cookie function returns true or false
+// if you have the cookie $("#splashscreen").removeClass("preload")
+// if you dont have the cookie  $("#splashscreen").addClass("preload")
 
-// })
-// $("body").on("resize", function () {
-//     responsive()
+// if function checkCookie is true remove class preload and empty splashscreen id, div. else create a cookie
+if (checkCookie()) {
+    $("#splashscreen").removeClass("preload")
+    $("#splashscreen").empty()
+} else {
+    $("#splashscreen").addClass("preload")
+    setCookie("splash", "screen", 1)
 
-// })
+}
 
-// function responsive() {
-// check if hamburger menu is open, i.e the #menu div has .active
-// if this is the case, make #content have left: 0px
+// document.cookie = "splash_user=yes"
+// let cookieValue = document.cookie
+// console.log(cookieValue)
 
-//otherwise, make #content have left: -150px
+// showSplash = () => {
 
-//     if ($("#menu").hasClass("active")) {
-//         $("#content").css({
-//             left: "-150px",
-//         })
-
-//     } else {
-//         $("#content").css({
-//             left: "0px",
-//         })
-
-//     }
+//     if (cookieValue)
 // }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var splash = getCookie("splash");
+    if (splash !== "") {
+        return true
+    } else {
+        return false
+    }
+}
--- tools/viewdns/script.js
+++ tools/viewdns/script.js
@@ -3,7 +3,6 @@
 //WORKS JUST NEEDS A NEW API KEY FROM VIEWDNS FROM AFTER 250 QUERIES
 
 
-alert("You must have the following to use this tool: Chrome browser, Access-Control-Allow-Origin Extenstion: https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf")
 
 $("#submit_dns").click(function () {
 
@@ -24,72 +23,18 @@ $("#submit_dns").click(function () {
 
     //creates a brand new query search for each individual type of dns recordd
 
-    let view_api_A = `https://api.viewdns.info/dnsrecord/?domain=${usr_domain}&recordtype=A&apikey=140f52607d22f4a79cd9931e57862ba98416eef7
-    &output=json`
+    let view_api_A = build_url(usr_domain, "A")
 
-    let view_api_CNAME = `https://api.viewdns.info/dnsrecord/?domain=${usr_domain}&recordtype=A&apikey=140f52607d22f4a79cd9931e57862ba98416eef7
-    &output=json`
+    let view_api_CNAME = build_url(usr_domain, "CNAME")
 
-    let view_api_MX = `https://api.viewdns.info/dnsrecord/?domain=${usr_domain}&recordtype=MX&apikey=140f52607d22f4a79cd9931e57862ba98416eef7
-    &output=json`
+    let view_api_MX = build_url(usr_domain, "MX")
 
-    let view_api_TXT = `https://api.viewdns.info/dnsrecord/?domain=${usr_domain}&recordtype=TXT&apikey=140f52607d22f4a79cd9931e57862ba98416eef7
-    &output=json`
+    let view_api_TXT = build_url(usr_domain, "TXT")
 
-    let view_api_SOA = `https://api.viewdns.info/dnsrecord/?domain=${usr_domain}&recordtype=SOA&apikey=140f52607d22f4a79cd9931e57862ba98416eef7
-    &output=json`
+    let view_api_SOA = build_url(usr_domain, "SOA")
 
+    let view_api_NS = build_url(usr_domain, "NS")
 
-    let view_api_NS = `https://api.viewdns.info/dnsrecord/?domain=${usr_domain}&recordtype=NS&apikey=140f52607d22f4a79cd9931e57862ba98416eef7&output=json`
-
-
-    //create a funciton that will show all records using the specified api url
-    Master_Lookup = (api_url) => {
-
-        $.get(api_url, function (response) {
-
-            let data = response
-
-            //get the domain name
-            let domain = data.query.domain
-
-            console.log(domain)
-
-
-            //get the dns type
-            let recordtype = data.query.recordtype
-            console.log("Record Type: " + recordtype)
-
-            let NameServerTitle = $("<h4>").attr({
-                style: "padding-top:10px",
-            }).html("- Record: " + recordtype + " -");
-
-            $("#dns_contents").append(NameServerTitle);
-
-
-            //show the dns servers found
-
-            //get contents inside of the records object
-            let servers = data.response.records
-
-            //console.log(servers)
-
-
-            for (let index = 0; index < servers.length; index++) {
-
-                //the data that needs to be shown on the page
-                let final_data = servers[index].data
-
-                let final = $("<p>").html(final_data);
-
-                $("#dns_contents").append(final);
-
-                console.log(servers[index].data)
-            }
-
-        })
-
-    }
 
     Master_Lookup(view_api_NS)
     Master_Lookup(view_api_A)
@@ -99,3 +44,62 @@ $("#submit_dns").click(function () {
     Master_Lookup(view_api_CNAME)
 
 })
+
+build_url = (domain, recordtype) => {
+
+    let url = `https://api.viewdns.info/dnsrecord/?domain=${domain}&recordtype=${recordtype}&apikey=140f52607d22f4a79cd9931e57862ba98416eef7
+&output=json`
+
+    url = encodeURI(url).replace(/&/g, "__")
+
+
+    return `https://backspacee.herokuapp.com/?url=${url}`;
+}
+
+//create a funciton that will show all records using the specified api url
+Master_Lookup = (api_url) => {
+
+    $.get(api_url, function (response) {
+
+        let data = response
+
+        //get the domain name
+        let domain = data.query.domain
+
+        console.log(domain)
+
+
+        //get the dns type
+        let recordtype = data.query.recordtype
+        console.log("Record Type: " + recordtype)
+
+        let NameServerTitle = $("<h4>").attr({
+            style: "padding-top:10px",
+        }).html("- Record: " + recordtype + " -");
+
+        $("#dns_contents").append(NameServerTitle);
+
+
+        //show the dns servers found
+
+        //get contents inside of the records object
+        let servers = data.response.records
+
+        //console.log(servers)
+
+
+        for (let index = 0; index < servers.length; index++) {
+
+            //the data that needs to be shown on the page
+            let final_data = servers[index].data
+
+            let final = $("<p>").html(final_data);
+
+            $("#dns_contents").append(final);
+
+            console.log(servers[index].data)
+        }
+
+    })
+
+}
